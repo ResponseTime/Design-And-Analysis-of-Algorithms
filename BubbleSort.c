51d682e0b0add7b0ac4fe9e413cb3d5732628004
Looper <aayush.bhattacharjee2002@gmail.com>
2023-02-22 13:47:05 +0530

    DAA5

--- BubbleSort.c
+++ BubbleSort.c
@@ -4,13 +4,13 @@ void bubble_sort(int *arr, int len)
 {
     for (int i = 0; i < len; i++)
     {
-        for (int j = 0; j < len; j++)
+        for (int j = 0; j < len - i - 1; j++)
         {
-            if (arr[i] < arr[j])
+            if (arr[j] > arr[j + 1])
             {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
